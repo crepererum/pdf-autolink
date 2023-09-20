a36c1eeb61b60ddd452e522ba874b928a77fc93a
Marco Neumann <marco@crepererum.net>
2023-09-20 21:46:48 +0200

    page table heuristic

--- src/pdf_autolink/__init__.py
+++ src/pdf_autolink/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from bisect import bisect
+from collections import defaultdict
 from dataclasses import dataclass
 import re
 
@@ -14,6 +15,7 @@ def run(file_in: str, file_out: str) -> None:
     print(f"text_len={len(idx.text)}")
     mark_pages(doc, idx)
     mark_chapters(doc, idx)
+    mark_page_tables(doc, idx)
     doc.save(file_out)
 
 
@@ -45,6 +47,13 @@ class TextIndex:
     text: str
     positions: list[TextPosition]
 
+    def find_indices(self, start: int, end: int) -> (int, int):
+        idx_start = bisect(self.positions, start, key=lambda p: p.txt_offset) - 1
+        idx_end = bisect(self.positions, end - 1, lo=idx_start, key=lambda p: p.txt_offset)
+        assert idx_end > idx_start
+
+        return (idx_start, idx_end)
+
 
 def mark_pages(doc: fitz.Document, index: TextIndex) -> None:
     for match in re.finditer(r"page\s+([0-9]+)", index.text, re.I):
@@ -55,7 +64,7 @@ def mark_pages(doc: fitz.Document, index: TextIndex) -> None:
         page_number = page_numbers[0]
 
         # build marker rects
-        create_links(doc, index, match.start(), match.end(), page_number, None)
+        create_links(doc, index, match.start(), match.end(), page_number)
 
 
 def mark_chapters(doc: fitz.Document, index: TextIndex) -> None:
@@ -75,12 +84,72 @@ def mark_chapters(doc: fitz.Document, index: TextIndex) -> None:
         create_links(doc, index, match.start(), match.end(), page_number, pos)
 
 
-def create_links(doc: fitz.Document, index: TextIndex, start: int, end: int, target_page: int, target_pos: fitz.Point | None):
+def mark_page_tables(doc: fitz.Document, index: TextIndex) -> None:
+    n_pages = len(doc)
+    pages = defaultdict(lambda: defaultdict(lambda: []))
+
+    # find potential links
+    for match in re.finditer(r"([a-z()]+\s+)*[a-z()]{2,}\s+([0-9]+)", index.text, re.I):
+        # find target page
+        page_label = match.group(2)
+        page_numbers = doc.get_page_numbers(page_label)
+        if len(page_numbers) != 1:
+            continue
+        page_number = page_numbers[0]
+
+        start = match.start()
+        end = match.end()
+        idx_start, idx_end = index.find_indices(start, end)
+
+        page = index.positions[idx_start].page
+        if page != index.positions[idx_end].page:
+            continue
+
+        pages[page][page_number].append((start, end))
+
+    # organize link source pages into runs
+    runs = []
+    run = None
+    for page in sorted(pages.keys()):
+        # filter out pages w/ not enough of links
+        if len(pages[page]) < 10:
+            continue
+
+        # add page to run
+        if run is None:
+            run = [page]
+        elif run[-1] + 1 == page:
+            run.append(page)
+        else:
+            runs.append(run)
+            run = [page]
+
+    # sort runs by number of outgoing unique links
+    runs = sorted(
+        runs,
+        key=lambda run: len({
+            target_page
+            for source_page in run
+            for target_page in pages[source_page].keys()
+        }),
+        reverse=True,
+    )
+
+    # emit two top-most runs
+    for run in runs[:2]:
+        for page in run:
+            print(f"found page table at page {page+1}")
+            page_entries = pages[page]
+            for page_number in sorted(page_entries.keys()):
+                targets = page_entries[page_number]
+                for start, end in targets:
+                    create_links(doc, index, start, end, page_number)
+
+
+def create_links(doc: fitz.Document, index: TextIndex, start: int, end: int, target_page: int, target_pos: fitz.Point | None = None):
     assert end > start
 
-    idx_start = bisect(index.positions, start, key=lambda p: p.txt_offset) - 1
-    idx_end = bisect(index.positions, end - 1, key=lambda p: p.txt_offset)
-    assert idx_end > idx_start
+    idx_start, idx_end = index.find_indices(start, end)
 
     marker_page = None
     marker_rect = None
